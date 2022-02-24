3c6a9da876a70b957541fda916c80847c08342cf
Jacob D <jwdonaldson99@gmail.com>
2022-02-24 11:33:15 -0700

    partial working category links

--- Zammers/Models/Basket.cs
+++ Zammers/Models/Basket.cs
@@ -1,6 +1,41 @@
-﻿namespace Zammers.Models
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Zammers.Models
 {
     public class Basket
-    {
+    {//create new list of line items
+        public List<BasketLineItem> Items { get; set; }
+        
+        public void AddItem (Book bk, int qty)
+        {//adds items to line item list with book ids
+            BasketLineItem Line = Items
+                .Where(b =>b.Book.BookId == bk.BookId)
+                .FirstOrDefault();
+            if (Line == null)
+            {
+                Items.Add(new BasketLineItem
+                    {//gets the total qty of books
+                    Book = bk,
+                    Quantity = qty
+                });
+            }
+            else
+            {
+                Line.Quantity += qty;
+            }
+          
+        }
+        public double CalcTotal()
+        {//cals total num of books
+            double sum = Items.Sum(b => b.Quantity * 25);
+            return sum;
+        }
+    }
+    public class BasketLineItem
+    {//line item id and quantity
+        public int LineID { get; set; }
+        public Book Book { get; set; }
+        public int Quantity { get; set; }
     }
 }
--- Zammers/Pages/bookcart.cshtml.cs
+++ Zammers/Pages/bookcart.cshtml.cs
@@ -1,15 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
+using Zammers.Infastructure;
+using Zammers.Models;
 
 namespace Zammers.Pages
 {
     public class bookcartModel : PageModel
     {
-        public void OnGet()
-        {
+        private IZammerRepo repo { get; set; }
+        public bookcartModel (IZammerRepo temp)
+        {//why do I do this. I think it is to create a new model based on the repo?
+            repo = temp;
         }
-        public void OnPost()
+        public Basket basket { get; set; }
+        public string ReturnUrl { get; set; }
+        public void OnGet(string returnUrl)
         {
+            ReturnUrl = returnUrl ?? "/";
+            basket = HttpContext.Session.GetJson<Basket>("basket") ?? new Basket();
+        }
+        public IActionResult OnPost(int bookId, string returnUrl)
+        {
+            Book b = repo.Books.FirstOrDefault(x => x.BookId == bookId);
+            basket = HttpContext.Session.GetJson<Basket>("basket") ?? new Basket();
+            basket.AddItem(b, 1);
+
+            HttpContext.Session.SetJson("basket", basket);
+
+            return RedirectToPage(new {ReturnUrl = returnUrl});
         }
     }
 }
