a3f2481333d939f0000cc65d4119182496f282c3
Jacob D <jwdonaldson99@gmail.com>
2022-02-24 11:02:50 -0700

    Category BUttons displayed

---
+++ Zammers/Components/CategoryViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Zammers.Models;
+
+namespace Zammers.Components
+{
+    public class CategoryViewComponent : ViewComponent 
+    {
+        private IZammerRepo repo2 { get; set; }
+
+        public CategoryViewComponent(IZammerRepo holder)
+        {//USes the Repo which sets up what we normally put here
+            repo2 = holder;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = repo2.Books
+                .Select(b => b.Category)
+                .Distinct()
+                .OrderBy(b => b);
+
+
+            return View(categories);
+        }
+    }
+}
