4f00e10ace10a4821f2e63b112c3c95866a3a5e9
Jacob D <jwdonaldson99@gmail.com>
2022-02-23 19:11:14 -0700

    Temp Fix

--- Zammers/Infastructure/UrlExtensions.cs
+++ Zammers/Infastructure/UrlExtensions.cs
@@ -2,7 +2,7 @@
 
 namespace Zammers.Infastructure
 {
-    public class UrlExtensions
+    public static class UrlExtensions
     {
         public static string PathAndQuery(this HttpRequest request) =>
             request.QueryString.HasValue ? $"{request.Path}{request.QueryString}" : request.PathAndQuery().ToString();
