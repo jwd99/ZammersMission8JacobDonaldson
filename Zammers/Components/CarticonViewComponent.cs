8a309e5e29f6e1d64ba6ce8edad9da2faed9ee07
Jacob D <jwdonaldson99@gmail.com>
2022-03-03 00:10:33 -0700

    Cart SUmmary stuff

---
+++ Zammers/Components/CarticonViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Zammers.Models;
+
+namespace Zammers.Components
+{
+    public class CarticonViewComponent : ViewComponent
+    {
+        private Basket basket;
+        public CarticonViewComponent(Basket basketservices)
+        {
+            basket = basketservices;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(basket);
+        }
+    }
+}
